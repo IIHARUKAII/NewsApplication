e3db888d7aa6c51e63dd067d0eb883a3cbb85d01
IIHARUKAII <sornsiri.kan@ku.th>
2021-03-09 21:39:02 +0700

    Initial commit

---
+++ lib/helper/data.dart
@@ -0,0 +1,42 @@
+import 'package:news/models/category_model.dart';
+
+List<CategoryModel> getCategories(){
+
+  List<CategoryModel> category = List<CategoryModel>();
+  CategoryModel categoryModel = new CategoryModel();
+
+  categoryModel.bannerName = "Apple";
+  categoryModel.categoryName = "Apple";
+  categoryModel.q = "apple";
+  categoryModel.imageUrl = "https://images.unsplash.com/photo-1614312385003-dcea7b8b6ab6?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1000&q=80";
+  category.add(categoryModel);
+
+  categoryModel = new CategoryModel();
+  //
+  categoryModel.bannerName = "Tesla";
+  categoryModel.categoryName = "Tesla";
+  categoryModel.q = "tesla";
+  categoryModel.imageUrl = "https://images.unsplash.com/photo-1553260188-75a8d6205b6c?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1000&q=80";
+  category.add(categoryModel);
+
+  categoryModel = new CategoryModel();
+  //
+  categoryModel.bannerName = "Microsoft";
+  categoryModel.categoryName = "Microsoft";
+  categoryModel.q = "microsoft";
+  categoryModel.imageUrl = "https://images.unsplash.com/photo-1496171367470-9ed9a91ea931?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1000&q=80";
+  category.add(categoryModel);
+
+  categoryModel = new CategoryModel();
+  //
+  // categoryModel.bannerName = "TechCrunch";
+  // categoryModel.categoryName = "apple";
+  // categoryModel.q = "techcrunch";
+  // categoryModel.imageUrl = "https://images.unsplash.com/photo-1518186285589-2f7649de83e0?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1000&q=80";
+  // category.add(categoryModel);
+  //
+  // categoryModel = new CategoryModel();
+
+  return category;
+
+}
---
+++ lib/helper/news.dart
@@ -0,0 +1,67 @@
+import 'dart:convert';
+
+import 'package:news/models/article_model.dart';
+import 'package:http/http.dart' as http;
+
+class News{
+
+  List<ArticleModel> news = [];
+  Future<void> getNews() async{
+    String url = "https://newsapi.org/v2/top-headlines?q=apple&apiKey=6e98598915704fb9aa83abea8f4a3d52";
+
+    var response = await http.get(url);
+
+    var jsonData = jsonDecode(response.body);
+
+    if(jsonData['status'] == "ok"){
+      jsonData["articles"].forEach((element){
+
+        if(element["urlToImage"] != null && element['description'] != null){
+
+          ArticleModel articleModel = ArticleModel(
+              title: element['title'],
+              author: element['author'],
+              description: element['description'],
+              url: element["url"],
+              urlToImage: element["urlToImage"],
+              content: element["content"]
+          );
+
+          news.add(articleModel);
+
+        }
+
+      });
+    }
+
+  }
+}
+
+class CategoryNewsClass {
+  List<ArticleModel> news = [];
+
+  Future<void> getNews(String q) async {
+    String url = "https://newsapi.org/v2/top-headlines?q=$q&apiKey=6e98598915704fb9aa83abea8f4a3d52";
+
+    var response = await http.get(url);
+
+    var jsonData = jsonDecode(response.body);
+
+    if (jsonData['status'] == "ok") {
+      jsonData["articles"].forEach((element) {
+        if (element["urlToImage"] != null && element['description'] != null) {
+          ArticleModel articleModel = ArticleModel(
+              title: element['title'],
+              author: element['author'],
+              description: element['description'],
+              url: element["url"],
+              urlToImage: element["urlToImage"],
+              content: element["content"]
+          );
+
+          news.add(articleModel);
+        }
+      });
+    }
+  }
+}
